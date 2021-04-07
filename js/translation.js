24639121956c4c97eecb8c585f0e297967dbf9b1
shoha-hakimjonov <nohajhohs@gmail.com>
2021-04-07 12:42:22 +0500

    first 3 pages done

---
+++ js/translation.js
@@ -0,0 +1,222 @@
+var app = {};
+app.translations = {
+
+  tj: {
+    english_lang: "Aнглисӣ",
+    tajik_lang: "Тоҷикӣ",
+    home: "Асосӣ",
+    about: "Дар бораи мо",
+    parents: "Барои Волидон",
+    educators: "Барои Муаллимон",
+    students: "Барои Хонандагон",
+    faq: "Саволҳо",
+    get_started: "Оғоз",
+
+    //main page
+    //slider 1
+    rs_1_layer_1: "Барои Хонандагон",
+    rs_1_layer_2: "Беҳтарин роҳи баланд бардоштани натиҷаҳои таълим",
+    rs_1_layer_3: "Пешрафти худро пайгирӣ кунед ва <br> тестҳои онлайнро пешниҳод кунед!",
+    go_to_students: "Ба хонандагон гузаред",
+
+    //slider 2
+    rs_2_layer_1: "Барои Муаллимон",
+    rs_2_layer_2: "Беҳтарин усули ҳамкорӣ бо хонандагони худ",
+    rs_2_layer_3: "Бо донишҷӯёни худ бо истифодаи мундариҷаи <br> викторинаҳо ва супоришҳо ҳамкорӣ кунед.",
+    go_to_teachers: "Ба муаллимон гузаред",
+
+    //slider 3
+    rs_3_layer_1: "Барои Волидон",
+    rs_3_layer_2: "Пешрафти фарзанди худро дастгирӣ кунед",
+    rs_3_layer_3: "Муваффақияти фарзанди худро <br> назорат кунед ва ба онҳо барои ба даст овардани бештар кӯмак расонед.",
+    go_to_parents: "Ба волидон гузаред",
+
+    welcome_to: "Хуш омадед ба",
+    mm_portal: "Maktab Mobile портали омӯзиши электронӣ",
+    available: "Барои ҳама донишҷӯён, омӯзгорон ва волидони Ҷумҳурии Тоҷикистон дастрас аст",
+    welcome_desc: "Maktab Mobile нахустин портали омӯзиши электронии Вазорати маориф ва илм мебошад, ки дар ҳама ҷо кор мекунад (компютерҳо, планшетҳо, телефонҳои мобилӣ) ҳам on-line ва ҳам ғоиб барои мусоидат ба омӯзиш ва дарёфти салоҳият.",
+    know_more: "Маълумоти бештар →",
+    what_can_you_do: 'Шумо чи метавонед карда ба воситаи <span class="text-theme-color-2 font-weight-400">Maktab Mobile',
+    access_learning_resources: "Дастрасӣ ба захираҳои омӯзишӣ",
+    acces_learning_desc: "Захираҳои омӯзиши рақамиро ба монанди китобҳои дарсӣ, видео, манбаъҳои хониш зеркашӣ ва истифода баред",
+    engage_in_learning: "Бо раванди таълим машғул шавед",
+    do_homework: "Вазифаи хонагӣ иҷро кунед ва ба донишҷӯён супориш диҳед",
+    asses_learning: "Муваффақиятҳои омӯзиширо арзёбӣ мекунанд",
+    asses_learning_desc: "Дастовардҳои синфии худро шарҳ диҳед",
+    review_progress: "Пешрафтро баррасӣ кунед",
+    review_progress_desc: "Пешрафти омӯзишро тавассути дастрасии фардӣ нишон диҳед",
+    how_do_reach_disconnected: 'Чи тавр Maktab Mobile ба одамони <span class="text-theme-color-2 font-weight-400">бе интернет</span> кумак мекунад?',
+    upload_content: "Маводҳоро баргузоред",
+    upload_content_desc: "Танҳо як бор мундариҷаро бор кунед ва истифодаи Maktab Mobile-ро ба таври мустақим идома диҳед",
+    ensure_sync: "Ҳамоҳангсозиро таъмин кунед",
+    ensure_sync_desc: "Ҳамоҳангсозии навсозиҳо дар байни донишҷӯён, ҳамсинфон ва ҳамсолони ғайримуқаррарии худро таъмин кунед",
+    use_peer: "ИСТИФОДАБАРИИ ҲАМСАФА",
+    use_peer_desc: "Корро идома диҳед ва мундариҷаро ғайримуқаррарӣ тавассути тақсимоти ҳамсол ба воситаи Bluetooth ё Wi-fi Direct мубодила кунед",
+    learning_package: "Бастаи омӯзишӣ",
+    learning_package_desc: "Маҷмӯи захираҳои фармоишии омодашуда барои ҳамкорӣ бо<br> донишҷӯён",
+    national_curriculum: "Барномаи миллии таълимӣ",
+    national_curriculum_desc: "Барномаи миллии таълимӣ аз ҷониби муаллимони касбӣ барои ҳамкории осон бо донишҷӯён таҳия шудааст",
+    additional_reading_resources: "Захираҳои иловагии хониш",
+    additional_reading_resources_desc: "Маҷмӯи манбаъҳои хониш барои таҳсил ва омӯзиш",
+    educational_video: "Захираҳои видеоии таълимӣ",
+    educational_video_desc: "Видеоҳое, ки аз ҷониби мутахассисон таҳия карда шудаанд, то каҷравии таълимро барои донишҷӯён сабуктар кунанд",
+
+    happy_students: "Донишҷӯёни хушбахт",
+    number_resources: "Шумораи захираҳо",
+    number_teachers: "Шумораи муаллимон",
+    awards_won: "Ҷоизаҳои ғолиб",
+    our_gallery: 'Галереяи<span class="text-theme-color-2 font-weight-400"> Мо</span>',
+    all: "Ҳама",
+    teachersg: "Муаллимон",
+    studentsg: "Донишҷуён",
+    parentsg: "Волидон",
+
+    partners: "Шарикон :",
+
+
+
+    //footer
+    maktab_desc_footer: "Maktab Mobile нахустин портали омӯзиши электронии Вазорати маориф ва илм мебошад, ки дар ҳама ҷо кор мекунад (компютерҳо, планшетҳо, телефонҳои мобилӣ) ҳам on-line ва ҳам ғайрирасмӣ барои омӯзиш ва дарёфти салоҳият мусоидат мекунанд.",
+    quick_links: "Истинодҳои зуд",
+    homef: "Асосӣ",
+    aboutf: "Дар бораи мо",
+    parentsf: "Барои Волидон",
+    teachersf: "Барои Муаллимон",
+    studentsf: "Барои Хонандагон",
+    faqf: "Саволҳо",
+    email: "Почтаи электронӣ",
+    dushanbe: "Душанбе, Тоҷикистон",
+
+    //about page
+    about_title: "Дар бораи мо",
+    homeb: "Асосӣ",
+    aboutb:"Дар бораи мо",
+    what_is_mm: "Maktab Mobile чист?",
+    what_is_mm_desc: "Мактаб Мобайл барномаест бо интерфейси оддӣ барои омӯзиши фосилавии хонандагон ва такмили ихтисоси омӯзгорон таҳия шудааст. ",
+    what_is_mm_desc2: "Мактаб Мобайл видео, китобҳои дасии электронӣ, маводи таълимии интерактивиро дастгирӣ намуда, тавассути он дастрасӣ ва мубодилаи мазмуну супоришҳои таълимӣ, идоракунии дарсҳо ва нақшаҳои дарс, маводу супоришҳо оид ба такмили ихтисоси омӯзгорон ба таври мустақим таъмин карда мешавад. “Мактаб Мобайл” ҳам онлайн ва ҳамчунон бе пайваствшавӣ ба интеренет фаъолият мекунад.",
+    who_can_benefit_from: "Ки аз Maktab Mobile фоида дида метавонад?",
+
+    teacherss: "Муаллимон",
+    teacherssb: "Муаллимон",
+    parentss: "Волидон",
+    parentssb: "Волидон",
+    studentss: "Хонандагон",
+    studentssb: "Хонандагон",
+    our_features: '<span class="text-theme-color-2 font-weight-400">Хусусиятҳои</span> мо',
+
+    libraryB: "Китобхона",
+    libraryBdesc: "Дастрасӣ ба корҳои курсиатон, супоришҳоро иҷро кунед ва китобҳои электронӣ, машқҳои интерактивӣ ё видеоҳоро аз китобхона интихоб кунед, то ки ба шумо дар омӯзиш кӯмак расонад.",
+
+    offline_support: "Дастгирии ғоиб",
+    offline_support_desc: "Ҳама чиз онлайн ё офлайн кор мекунад. Ҳангоми пайвастшавӣ холҳо ва пешниҳодҳои шумо ба таври худкор ҳамоҳанг мешаванд.",
+
+    interactive_learning: "Дастгирии омӯзиши интерактивӣ",
+    interactive_learning_desc: "Мазмуни таълимиро ба осонӣ илова кунед, нест кунед ва таҳрир кунед. Дастгирии дарунсохти мундариҷаи мавҷудаи шумо, аз ҷумла видео, аудио, EPUB, PDF ва Experience API.",
+
+    class_management: "Идоракунии синфҳо",
+    class_management_desc: "Супоришҳои синфӣ эҷод кунед, пешрафти донишҷӯёнро пайгирӣ кунед ва фикру мулоҳизаҳои худро барои донишҷӯён сабт кунед.",
+
+    attendance: "Давомот",
+    attendance_desc: "Тафсилоти дарсҳо, тақвимҳо ва ҳузури сабтро идора кунед.",
+
+    reports: "Ҳисоботҳо",
+    reports_desc: "Гузориши муассир, содда ва мустақим аз ҷониби муаллимон ва донишҷӯён.",
+
+    //for teachers
+    for_teachers_b : "Барои Муаллимон",
+    homeb : "Асосӣ",
+    teachersb: "Барои Муаллимон",
+    how_do_teachers_use: 'Муаллимон аз <span class="text-theme-color-2 font-weight-400">Maktab Mobile</span> чӣ гуна истифода мебаранд?',
+    how_do_teachers_use_desc: "",
+
+    how_to_download_and_register: 'Чӣ тавр  <span class="text-theme-colored"> барнома дар дастгоҳи худ зеркашӣ </span><br> ва сабти ном карда мешавад?',
+    download_1: "Қадами 1: Ба Гугл Плей/Плей Маркет гузаред ва барномаи “Мактаб Мобайл” -ро зеркашӣ кунед. ",
+    download_2: "Қадами 2: Дархости рамзи синф аз омӯзгори худ. ",
+    download_3: "Қадами 3: Ном ва дигар маълумот барои тамосро ба қайд гиред.",
+    go_to_google1: "Ба Google Play гузаред",
+
+    how_to_create_class: 'Чӣ гуна <span class="text-theme-colored"> бояд синф эҷод кард </span><br> ва донишҷӯёнро илова кард?',
+    how_to_create_class_desc: 'Ба қисмати "Илова кардани синф" гузаред ва тафсилоти синфро, ки ба он дарс медиҳед, ворид кунед,. Дар қисмати "Номи синф" маълумоти синфи худро ворид кунед.',
+    go_to_google2: "Ба Google Play гузаред",
+
+    how_to_create_classwork: 'Кори синфиро <span class="text-theme-colored"> чӣ гуна бояд сохт?',
+    how_to_create_classwork_desc: 'Барои ворид намудани кори синфӣ, тугмаи "+ кори синфӣ" -ро зер кунед. Сарлавҳаи синфро пур кунед ва мӯҳлати иҷро/пешниҳоди онро гузоред. Инчунин шумо бояд муайян кунед, ки аз хонандагони худ кадом навъи ҷавобу пешниҳодро интизоред. Дар қисмати "Дастур барои хонандагон" ба кӯдакон дастурҳои аниқ диҳед, ки чӣ гуна ҷавобу пешниҳоди худро омода кунанд. Кори синфӣ ихтиёрӣ метавонад саволҳои викторина дошта бошад, ки хонандагон дар қисмати "Шарҳи озод" ҷавоб дода, пешниҳод кунанд.',
+    go_to_google3: "Ба Google Play гузаред",
+
+    how_to_see_student_progress: 'Пешрафти хонандаро <span class="text-theme-colored"> чӣ гуна дидан мумкин аст  </span><br> ва супоришҳоро чӣ тавр қайд кардан мумкин аст?',
+    how_to_see_student_progress_desc: 'Барои шарҳи пешрафти хоандагон ва якчанд супоришҳоро қайд кардан, ба қисмати "Пешрафти хонандагон" гузаред. Он ба шумо шумораи супоришҳои "Қайдшуда", "Пешниҳодшуда" ва "Пешниҳод нашудааст" -ро нишон медиҳад. Пас аз он ки шумо пешниҳоди хонандагонро баррасӣ карда, аломат гузоштед, тугмачаи "Бозгашт ва Қайди минбаъда" -ро зер кунед.',
+    go_to_google4: "Ба Google Play гузаред",
+
+    how_to_send_comments: 'Чӣ гуна <span class="text-theme-colored"> шарҳҳоро ба донишҷӯён фиристодан мумкин аст? </span>',
+    how_to_send_comments_desc: 'Ду роҳи фиристодани эродҳо/фикру мулоҳизаҳо вуҷуд дорад: ба тамоми синф ва фардӣ ба хонандагони алоҳида. Барои илова намудани шарҳ ба тамоми синф ба фосилаи "Шарҳи синф илова кунед" равед. Барои илова кардани шарҳи хусусӣ ба донишҷӯёни инфиродӣ, ба фосилаи "Шарҳи хусусӣ илова кунед" равед.',
+    go_to_google5: "Ба Google Play гузаред",
+
+    general_tips_for_teachers: 'Маслиҳатҳои умумӣ барои омӯзгорон оид ба истифодаи барнома',
+    tip1: 'Боварӣ ҳосил намоед, ки дастгоҳи шумо барои дастрасӣ ба ҳамаи дарсҳои барнома дар ҳолати корӣ қарор дорад.',
+    tip2: "Боварӣ ҳосил кунед, ки омӯзгорон ва падару модарони онҳо рақами шахсии шумо (ё Viber, рақами вотсап) дошта бошанд, то ҳар вақте ки онҳо ҳангоми иҷрои супоришҳо ба мушкилӣ дучор  дучор шаванд, бо шумо тамос гиранд",
+
+    try_now: "Гузаред",
+
+  }
+};
+setLanguage(getCookie('lang') || 'en');
+function setLanguage(language) {
+  var x = getCookie('lang');
+  if (x) {
+    if (x != language) {
+      eraseCookie('lang');
+      setCookie('lang', language, 7);
+    }
+  } else {
+    setCookie('lang', language, 7);
+  }
+  document.documentElement.lang = language;
+  var currentLanguage = getCookie('lang');
+
+  app.lang = app.translations[currentLanguage] || app.translations.en;
+  //changeLogo(currentLanguage);
+  for (const property in app.lang) {
+    // console.log(`${property}: ${app.lang[property]}`);
+    if (document.getElementById(property) != null && currentLanguage != "en") {
+      document.getElementById(property).innerHTML = app.lang[property];
+    //  document.getElementById(property).style.fontFamily = 'OpenSans';
+    }
+  }
+}
+
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+function eraseCookie(name) {
+  document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+function changeLogo(lang) {
+  var img = document.getElementById("logo_img");
+  if(lang == 'en'){
+    img.src = "img/logo_en.png";
+  }else if(lang=='ru'){
+    img.src = "img/logo_ru.png";
+  }else if(lang=='tj'){
+    img.src = "img/logo_tj.png";
+  }
+/*var src = document.getElementById("logo_a");
+src.appendChild(img);*/
+}
+
